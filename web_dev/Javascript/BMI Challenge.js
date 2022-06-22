ad1aaefbe09c2f0cb2afd394bba797062cca957a
AurangKhaliq <97285078+AurangKhaliq@users.noreply.github.com>
2022-06-22 18:24:48 +0100

    Add files via upload

---
+++ web_dev/Javascript/BMI Challenge.js
@@ -0,0 +1,6 @@
+function bmiCalc(wright, height){
+    
+    var bmi = weight/(height*height);
+
+    return(math.round(bmi));
+}
---
+++ web_dev/Javascript/condition.js
@@ -0,0 +1,59 @@
+
+// var year = 2022;
+// debugger;
+// if (year % 4 === 0) {
+//     if (year % 100 === 0) {
+//         if (year % 400 === 0) {
+//             console.log("The year   " + year + "   is a leap year  ");
+//         }
+//         else {
+//             console.log("The year  " + year + "   is not a leap year");
+//         }
+//     }
+//     else {
+//         console.log("The year  " + year + "  it is a leap year");
+//     }
+// }
+// else {
+//     console.log("The year   " + year + "   is not a leap year");
+// }
+
+
+// var num = 0;
+// do {
+//     console.log(num);
+//     num++;
+// } while(num<=10)
+
+// var num=0;
+// while (num<=10){
+//     console.log(num)
+//     num++;
+// }
+
+// for (var num=1; num<=10; num++){
+//     console.log(num)
+// }
+
+// for(var num=1; num<=10; num++){
+//     console.log(num+"x 8 ="+num*8)
+// }
+
+
+// function sum(a,b) {
+// var a=10; b=10;
+
+// var total = a+b;
+// console.log(total)
+// }
+
+// var a=10; b=10;
+// sum(a,b);
+
+function sum(a,b) {
+    return total=a+b;
+}
+
+var total_1 = sum(5,25);
+
+console.log(total_1);
